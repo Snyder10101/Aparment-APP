70b6b3d54de391495c58c4df73d877b0b81520a6
Christian Snyder <Christiansnyder562@gmail.com>
2023-02-08 14:59:40 -0800

    improvment

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,11 +1,34 @@
-import React from "react"
+import React from "react";
+import { Button } from "reactstrap";
 
-const Home = () => {
+const Home = ({ new_user_route, sign_in_route }) => {
   return (
-    <>
-      <h3>Home</h3>
-    </>
-  )
-}
+    // if (authenticated) {
+    //   <SignedInView /> // this would be user feed page with bunch of dynamic content
+    // } else {
+    //   <SignedOutView /> // this would be a splash page
+    // }
+    <div
+      style={{
+        height: "25rem",
+        width: "50rem",
+        border: "1px solid black",
+        background: "#d8d8d8",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <h1>Welcome to Raymonds Friends Apts</h1>
+      <p>Place holder</p>
+      <Button href={new_user_route} className="nav-link">
+        Sign Up
+      </Button>
+      <Button onClick={sign_in_route} className="nav-link">
+        Sign In
+      </Button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
