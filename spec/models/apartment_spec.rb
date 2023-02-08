a3ff1a31de452d41833c875a2f600281c020500f
RaymondOchoa <ochoaraymondb@yahoo.com>
2023-02-08 12:16:32 -0800

    identation updated

--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Apartment, type: :model do
     it 'must have a street' do
-        apartment = Apartment.create 
+        apartment = Apartment.create(
         street: "", 
         city: "America Town", 
         state: "CA", 
@@ -13,11 +13,11 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:street]).to_not be_empty
     end
     it 'must have a city' do
-        apartment = Apartment.create 
+        apartment = Apartment.create(
         street: "1", 
         city: "", 
         state: "CA", 
@@ -27,11 +27,12 @@ RSpec.describe Apartment, type: :model do
         bedrooms: "3", 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: '1'
+        user_id: '1')
         expect(apartment.errors[:city]).to_not be_empty
     end
     it 'must have a state' do
-        apartment = Apartment.create street: '1', 
+        apartment = Apartment.create(
+        street: '1', 
         city: "America Town", 
         state: "", 
         manager: "Ryan", 
@@ -41,11 +42,12 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:state]).to_not be_empty
     end
     it 'must have a manager' do
-        apartment = Apartment.create street: "1", 
+        apartment = Apartment.create( 
+        street: "1", 
         city: "America Town", 
         state: "CA", 
         manager: "", 
@@ -55,11 +57,11 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:manager]).to_not be_empty
     end
     it 'must have a email' do
-        apartment = Apartment.create 
+        apartment = Apartment.create(
         street: "1", 
         city: "America Town", 
         state: "CA", 
@@ -70,11 +72,11 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:email]).to_not be_empty
     end
     it 'must have a price' do
-        apartment = Apartment.create 
+        apartment = Apartment.create( 
         street: "1", 
         city: "America Town", 
         state: "CA", 
@@ -85,11 +87,11 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:price]).to_not be_empty
     end
     it 'must have a bedrooms' do
-        apartment = Apartment.create 
+        apartment = Apartment.create( 
         street: "1", 
         city: "America Town", 
         state: "CA", 
@@ -100,11 +102,11 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:bedrooms]).to_not be_empty
     end
     it 'must have a bathrooms' do
-        apartment = Apartment.create 
+        apartment = Apartment.create( 
         street: "1", 
         city: "America Town", 
         state: "CA", 
@@ -115,11 +117,11 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '', 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:bathrooms]).to_not be_empty
     end
     it 'must have a pets' do
-        apartment = Apartment.create 
+        apartment = Apartment.create( 
         street: "1", 
         city: "America Town", 
         state: "CA", 
@@ -130,11 +132,11 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "", 
         image: "http:url.com", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:pets]).to_not be_empty
     end
     it 'must have a image' do
-        apartment = Apartment.create 
+        apartment = Apartment.create( 
         street: "1", 
         city: "America Town", 
         state: "CA", 
@@ -145,11 +147,11 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "yes", 
         image: "", 
-        user_id: "1"
+        user_id: "1")
         expect(apartment.errors[:image]).to_not be_empty
     end
     it 'must have a user_id' do
-        apartment = Apartment.create 
+        apartment = Apartment.create(
         street: "1", 
         city: "America Town", 
         state: "CA", 
@@ -160,7 +162,7 @@ RSpec.describe Apartment, type: :model do
         bathrooms: '2', 
         pets: "yes", 
         image: "http:url.com", 
-        user_id: ""
+        user_id: "")
         expect(apartment.errors[:user_id]).to_not be_empty
     end
 end
